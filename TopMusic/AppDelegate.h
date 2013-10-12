5bedf550cbe9a489870d0cb4e875706e3cf85467
Alfredo Arias <alfre.arias@gmail.com>
2013-10-12 12:30:33 -0400

    Initial Commit

---
+++ TopMusic/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TopMusic
+//
+//  Created by Tekhne on 10/12/13.
+//  Copyright (c) 2013 Tekhne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TopMusic/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TopMusic
+//
+//  Created by Tekhne on 10/12/13.
+//  Copyright (c) 2013 Tekhne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
